28acb76357dea69dad4203d8f19af9dd685e7ced
sanrala <104975220+sanrala@users.noreply.github.com>
2024-05-31 17:07:07 +0200

    battlenet

--- src/Components/Precommandes/Precommandes.jsx
+++ src/Components/Precommandes/Precommandes.jsx
@@ -38,11 +38,11 @@ function Precommandes() {
           pathname: `/PrecoFull/`,
         }}
       >
-      <h3 class="nk-decorated-h-2">
-        <span>
-          <span class="text-main-1">Jeux</span> en précommandes
-        </span>
-      </h3>
+        <h3 class="nk-decorated-h-2">
+          <span>
+            <span class="text-main-1">Jeux</span> en précommandes
+          </span>
+        </h3>
       </Link>
       <div class="nk-gap"></div>
       <div class="nk-blog-grid">
@@ -50,10 +50,116 @@ function Precommandes() {
           {preco.map((item) => (
             <>
               {/* {item.precommande === true && ( */}
-                <div class="col-md-6 col-lg-4" key={item.id}>
-                  {/* <!-- START: Post --> */}
+              <div class="col-md-6 col-lg-4" key={item.id}>
+                {/* <!-- START: Post --> */}
 
-                  <div class="nk-blog-post" key={item.id}>
+                <div class="nk-blog-post" key={item.id}>
+                  {item.plateformes &&
+                    item.plateformes.map((v, index) => (
+                      <>
+                        {v.support === "Steam" && (
+                          <Link
+                            key={item.id}
+                            {...item}
+                            to={{
+                              pathname: `/PC/${item.id}/${item.support}`,
+                              state: { itemData: item }, // Passer les données de l'élément à la page BlocArticle
+                            }}
+                            class="nk-post-img"
+                          >
+                            <HoverVideoPlayer
+                              className="tot"
+                              videoSrc={item.videoHover}
+                              style={{
+                                // Make the image expand to cover the video's dimensions
+                                width: '100%',
+                                height: '100%',
+                                objectFit: 'cover',
+                              }}
+                              pausedOverlay={
+                                <img src={item.imageUrl} alt={item.title} />
+                              }
+
+                            />
+                            {/* <img src={item.imageUrl} alt={item.title} /> */}
+                            <span class="nk-post-comments-count">{item.promo}</span>
+
+                            <span class="nk-post-categories">
+                              <span class="bg-main-5">{item.genre}</span>
+                            </span>
+                          </Link>
+                        )}
+                        {v.support === "Rockstar" && (
+                       <Link
+                       key={item.id}
+                       {...item}
+                       to={{
+                         pathname: `/PC_Rockstar/${item.id}/${item.support}`,
+                         state: { itemData: item }, // Passer les données de l'élément à la page BlocArticle
+                       }}
+                       class="nk-post-img"
+                     >
+                       <HoverVideoPlayer
+                         className="tot"
+                         videoSrc={item.videoHover}
+                         style={{
+                           // Make the image expand to cover the video's dimensions
+                           width: '100%',
+                           height: '100%',
+                           objectFit: 'cover',
+                         }}
+                         pausedOverlay={
+                           <img src={item.imageUrl} alt={item.title} />
+                         }
+
+                       />
+                       {/* <img src={item.imageUrl} alt={item.title} /> */}
+                       <span class="nk-post-comments-count">{item.promo}</span>
+
+                       <span class="nk-post-categories">
+                         <span class="bg-main-5">{item.genre}</span>
+                       </span>
+                     </Link>
+                        )
+                        }
+                        {v.support === "Battle.net" && (
+                           <Link
+                           key={item.id}
+                           {...item}
+                           to={{
+                             pathname: `/Battlenet/${item.id}/${item.support}`,
+                             state: { itemData: item }, // Passer les données de l'élément à la page BlocArticle
+                           }}
+                           class="nk-post-img"
+                         >
+                           <HoverVideoPlayer
+                             className="tot"
+                             videoSrc={item.videoHover}
+                             style={{
+                               // Make the image expand to cover the video's dimensions
+                               width: '100%',
+                               height: '100%',
+                               objectFit: 'cover',
+                             }}
+                             pausedOverlay={
+                               <img src={item.imageUrl} alt={item.title} />
+                             }
+
+                           />
+                           {/* <img src={item.imageUrl} alt={item.title} /> */}
+                           <span class="nk-post-comments-count">{item.promo}</span>
+
+                           <span class="nk-post-categories">
+                             <span class="bg-main-5">{item.genre}</span>
+                           </span>
+                         </Link>
+                        )
+                        }
+                      </>
+                    ))}
+
+                  <div class="nk-gap"></div>
+                  <span class="nk-post-title h4">
                     <Link
                       key={item.id}
                       {...item}
@@ -61,60 +167,28 @@ function Precommandes() {
                         pathname: `/PC/${item.id}/${item.title}`,
                         state: { itemData: item }, // Passer les données de l'élément à la page BlocArticle
                       }}
-                      class="nk-post-img"
                     >
-                      <HoverVideoPlayer
-                        className="tot"
-                        videoSrc={item.videoHover}
-                        style={{
-                          // Make the image expand to cover the video's dimensions
-                          width: '100%',
-                          height: '100%',
-                          objectFit: 'cover',
-                        }}
-                        pausedOverlay={
-                          <img src={item.imageUrl} alt={item.title} />
-                        }
-                       
-                      />
-                      {/* <img src={item.imageUrl} alt={item.title} /> */}
-                      <span class="nk-post-comments-count">{item.promo}</span>
-
-                      <span class="nk-post-categories">
-                        <span class="bg-main-5">{item.genre}</span>
-                      </span>
+                      {item.title.slice(0, 17) + "..."}
                     </Link>
-                    <div class="nk-gap"></div>
-                    <span class="nk-post-title h4">
-                      <Link
-                        key={item.id}
-                        {...item}
-                        to={{
-                          pathname: `/PC/${item.id}/${item.title}`,
-                          state: { itemData: item }, // Passer les données de l'élément à la page BlocArticle
-                        }}
-                      >
-                        {item.title.slice(0, 17) + "..."}
-                      </Link>
-                    </span>
-                    {/* <div class="nk-post-text">
+                  </span>
+                  {/* <div class="nk-post-text">
                      
                       <p>{item.resume.slice(0, 190) + "..."}</p>
                     </div> */}
-                    <div class="nk-gap"></div>
-                    <div className="d-flex justify-content-between text-white">
-                      <div className="div">
-                        <span class="preco ">PRECO </span>{" "}
-                        <span class="preco__date">{formatDate(item.dateSortie)}</span>
-                        {/* <div class="nk-post-date float-right"> */}
-                      </div>
-                      {item.price}
-                      {/* </div> */}
+                  <div class="nk-gap"></div>
+                  <div className="d-flex justify-content-between text-white">
+                    <div className="div">
+                      <span class="preco ">PRECO </span>{" "}
+                      <span class="preco__date">{formatDate(item.dateSortie)}</span>
+                      {/* <div class="nk-post-date float-right"> */}
                     </div>
+                    {item.price}
+                    {/* </div> */}
                   </div>
-
-                  {/* <!-- END: Post --> */}
                 </div>
+
+                {/* <!-- END: Post --> */}
+              </div>
               {/* )} */}
             </>
           ))}
