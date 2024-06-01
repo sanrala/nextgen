d2aecbbb93c76796942734e7d49a216209fef9fc
sanrala <sebastien.mauro@gmail.com>
2024-06-01 18:15:53 +0200

    vendor

--- src/Components/Precommandes/Precommandes.jsx
+++ src/Components/Precommandes/Precommandes.jsx
@@ -2,7 +2,7 @@ import React, { useState, useMemo, useEffect } from "react";
 import { Link } from "react-router-dom";
 import data from "./../../games.json";
 import HoverVideoPlayer from "react-hover-video-player";
-
+import Ho from "./../../assets/video/Homeworld3.mp4"
 function Precommandes() {
   const [games, setGames] = useState([]);
 
