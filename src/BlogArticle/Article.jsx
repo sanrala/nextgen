21b2101c9a47045d0008debb46a53462238cb084
sanrala <104975220+sanrala@users.noreply.github.com>
2026-05-27 15:16:54 +0200

    Update Article.jsx

--- src/BlogArticle/Article.jsx
+++ src/BlogArticle/Article.jsx
@@ -1,365 +1,111 @@
-
-// import React, { useState, useMemo, useEffect } from "react";
 import Header from "./../Components/Header/Header";
 import { useParams, Link } from "react-router-dom";
-// import { Link, useParams, useLocation } from "react-router-dom";
 import Footer from "./../Components/Footer/Footer";
 import gameData from "./../games.json";
 import Box from "@mui/material/Box";
-
 import CircularProgress from '@mui/material/CircularProgress';
 
 function BlocArticle() {
+
   function formatDate(dateString) {
     const date = new Date(dateString);
     const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
     return date.toLocaleDateString('fr-FR', options);
   }
-  const { news_id, title, id } = useParams();
-  const article = gameData.find(news => news.id === parseInt(id)); // Recherchez l'article correspondant dans le JSON
+
+  const { news_id, id } = useParams();
+  const article = gameData.find(news => news.id === parseInt(id));
 
   if (!article) {
-    return <div>Article non trouvé</div>; // Gérez le cas où l'article n'est pas trouvé
+    return <div>Article non trouvé</div>;
   }
-  console.log(news_id);
-  console.log(title);
+
   return (
     <div>
       <Header />
-      <div class="nk-gap-1"></div>
-      <div class="container">
-        {article ? (
-          <ul class="nk-breadcrumbs">
-            <li>
-              <Link
-                to={{
-                  pathname: `/`,
-                  // Passer les données de l'élément à la page BlocArticle
-                }}
-              >
-                Accueil{" "}
-              </Link>
-            </li>
 
-            <li>
-              <span class="fa fa-angle-right"></span>
-            </li>
-            <li>
-              <Link to="/...">Actualité</Link>
-            </li>
-            <li>
-              <span class="fa fa-angle-right"></span>
-            </li>
-            <li>
-            {article.plateformes &&
-              article.plateformes.map((v, index) => (
-                <>
-                {v.support === "Steam" && ( 
-            <Link
-                    
-                    key={article.id}
-                    {...article}
-                    
-                    to={{
-                      pathname: `/PC/${article.id}/${article.title}`,
-                      state: { itemData: article }, // Passer les données de l'élément à la page BlocArticle
-                    }}
-                   >{article.title}</Link>
-                )} 
-                
-                {v.support === "Rockstar" && ( 
-                  <Link
-                    
-                  key={article.id}
-                  {...article}
-                  
-                  to={{
-                    pathname: `/PC_Rockstar/${article.id}/${article.title}`,
-                    state: { itemData: article }, // Passer les données de l'élément à la page BlocArticle
-                  }}
-                 >{article.title}</Link>
-                )
-                  }
-                    {v.support === "Battle.net" && ( 
-                  <Link
-                    
-                  key={article.id}
-                  {...article}
-                  
-                  to={{
-                    pathname: `/Battlenet/${article.id}/${article.title}`,
-                    state: { itemData: article }, // Passer les données de l'élément à la page BlocArticle
-                  }}
-                 >{article.title}</Link>
-                )
-                  }
-                   </>
-                  ))}
+      <div className="container">
+        <ul className="nk-breadcrumbs">
+          <li><Link to="/">Accueil</Link></li>
+          <li><span className="fa fa-angle-right"></span></li>
+          <li><Link to="/actualite">Actualité</Link></li>
+          <li><span className="fa fa-angle-right"></span></li>
 
-            </li>
-        
-          </ul>
-        ) : (
-          // Code à exécuter lorsque item est null
-          <Box sx={{ display: 'flex' }}>
-            <CircularProgress />
-          </Box>
-        )}
+          <li>
+            {article.title}
+          </li>
+        </ul>
       </div>
-      <div class="nk-gap-1"></div>
-      {/* // <!-- END: Breadcrumbs --> */}
 
-      <div class="nk-gap-2"></div>
+      <div className="container">
+        <div className="row vertical-gap">
+          <div>
 
-      <div class="container">
-        {article ? (
-          <div class="row vertical-gap">
-            <div class="">
-              {/* <!-- START: Post --> */}
-              {/* <img
-                class="nk-page-backgroundBlog-top"
-                src={item.backImage}
-                alt=""
-              /> */}
-              {article.news && article.news.map((item, id) => (
-                <>
-                  {item.news_id !== news_id ? (
-                    null) :
-                    (
-                      <div class="nk-blog-post nk-blog-post-single">
-                        {/* <!-- START: Post Text --> */}
-                        <div class="nk-post-text mt-0">
-                          <div className="panel   ">
-                            <div class="nk-post-imgBloc">
-                              <img src={article.imageUrl} alt={article.title} className="img-fluid"/>
+            {article.news && article.news.map((item, index) => (
+              item.news_id === news_id && (
 
+                <div key={index} className="nk-blog-post nk-blog-post-single">
 
-                              <div className="priceBloc d-flex flex-start flex-column justify-content-center ">
+                  <div className="nk-post-text mt-0">
 
-                                {/* <h3 class="nk-post-title ">{article.title}</h3> */}
-                                <div class="subinfos">
+                    <img src={article.imageUrl} alt={article.title} className="img-fluid"/>
 
+                    <p>Sortie : {formatDate(article.dateSortie)}</p>
 
-                                  <div class="single platform text-white">
-                                    <span class="fa fa-calendar"></span>
-                                  </div>
-                                  Sortie
-                                  <div class="spacer"></div>
+                    <h4>{item.title}</h4>
+                    <p>{item.new}</p>
 
-                                  <div class="preorder text-danger">
-                                    {formatDate(article.dateSortie)}{" "}
-                                  </div>
-                                </div>
-                                <div class="nk-post-categories d-flex align-items-center flex-column">
-                                  {article.about &&
-                                    article.about.map((ab, index) => (
-                                      <>
-                                        <div className="cat">
-                                          <p className="ss_cat">Développeur :    </p>
-                                          <p className="ss_cat_rep"> {ab.dev}</p>
-                                        </div>
+                    <img src={item.imageUrl} alt={item.title} className="img-fluid" />
 
-                                        <div className="cat">
-                                          <p className="ss_cat">Editeur :    </p>
-                                          <p className="ss_cat_rep"> {ab.editeur}</p>
-                                        </div>
+                    {/* 🔥 FIX LINK */}
+                    {item.LinkUrl && (
+                      <a href={item.LinkUrl} className="know">
+                        {item.Link}
+                      </a>
+                    )}
 
-                                        <div className="cat">
-                                          <p className="ss_cat">Genres :    </p>
-                                          <p className="ss_cat_rep"> {article.genres}</p>
-                                        </div>
-
-                                      </>
-                                    ))}
-                             
-
-                                </div>
-                              </div>
-
-
-                            </div>
-                          </div>
-
-                          <div class="separator product-panel"></div>
-
-                          <ul class="nk-breadcrumbs">
-                            <li>
-                              <span>Actualité</span>
-                            </li>
-                          </ul>
-                          <div class="separator product-panel"></div>
-
-                          <div className="panel   ">
-                            <div class="nk-post-imgBloc">
-
-                              <div class="text readable">
-                                <h4>{item.title}</h4>
-                                {item.new}</div>{" "}
-
-                              <div class="nk-post-categories d-flex align-items-center">
-
-                                <img src={item.imageUrl} alt={item.title} className="img-fluid" />
-                                
-                              </div>
-                            </div>
-                          </div>
-                          <div class="nk-gap-2"></div>
-                          <div className="text-center">
-                            {item.new2 != undefined || item.new3 != undefined || item.title2 != undefined || item.img != undefined || item.Link != undefined || item.LinkUrl != undefined ? (
-                              <div >
-                                <h3 className="h4">{item.title2}</h3>
-                                
-                                <img className="img-fluid " src={item.img}  />
-                                <div class="nk-gap-2"></div>
-                                <p>{item.new2}</p>
-                                <p>{item.new3}</p>
-                                {item.LinkUrl ? (
-                    <a href={item.LinkUrl} className="know">{item.Link}</a>
-                ) : (
-                    <a className="know" style={{ display: 'none' }}>{item.Link}</a>
-                )}
-                              </div>
-                            ) : (
-                              <div className="div"></div>
-
-                            )}
-                          </div>
-                          <div class="separator product-panel"></div>
-                          {item.video !== null ? (
-                            <>
-                              <ul class="nk-breadcrumbs">
-                                <li>
-                                  <span>Media</span>
-                                </li>
-                              </ul>
-                              <div class="separator product-panel"></div>
-                              <div className="video-container">
-                                <iframe
-                                  title="YouTube Video"
-                                  src={`https://www.youtube.com/embed/${item.video}`} // Utilisation de la variable videoId pour dynamiquement spécifier l'URL de la vidéo
-                                  frameBorder="0"
-                                  allowFullScreen
-                                ></iframe>
-                              </div>
-                            </>
-                          ) : null
-                          }
-                          <div class="nk-gap-2"></div>
-
-
-                          <h3 class="nk-decorated-h-2">
-              <span>
-                <span class="text-main-1">Jeux</span> Similaires
-              </span>
-            </h3>
-
-            <div class="nk-gap"></div>
-            <div className="slider-container">
-              {/* <Slider {...settings}> */}
-              {gameData.map((i, id) => (
-                <div
-                  className="nk-blog-poste"
-                  key={id}
-                  style={{
-                    display: article.genre !== i.genre ? "none" : "block",
-                  }}
-                  
-                >
-                  {article.genre === i.genre ? (
-                    <div
-                      key={id}
-                      // style={{ width: '40%' }}
-                    >
-                     <Link
-                    
-                  key={i.id}
-                  {...i}
-                  
-                  to={{
-                    pathname: `/PC/${i.id}/${i.title}`,
-                    state: { itemData: i }, // Passer les données de l'élément à la page BlocArticle
-                  }}
-                  onClick={() => {
-                    window.scrollTo(0, 0)
-                  }}
-                  class="nk-post-img">
-                        <img src={i.imageUrl} alt={i.title} className="img-fluid" />
-                        <span className="nk-post-comments-count">
-                          {i.promo}
-                        </span>
-                        </Link>
-                      <div className="nk-gap"></div>
-                      <h2 className="nk-post-title h4 d-flex justify-content-between">
-                      <Link
-                  
-                  key={i.id}
-                  {...i}
-                  to={{
-                    pathname: `/PC/${i.id}/${i.title}`,
-                    state: { itemData: i }, // Passer les données de l'élément à la page BlocArticle
-                  }}
-                  class="nk-post-img">{i.title}  </Link>
-                        <span>{i.price}</span>
-                      </h2>
-                    </div>
-                  ) : null}
-                </div>
-              ))}
-              {/* </Slider> */}
-            </div>
-
-
-
-                        </div>
-
-
-
-
-
-
+                    {/* VIDEO */}
+                    {item.video && (
+                      <div className="video-container">
+                        <iframe
+                          title="YouTube Video"
+                          src={`https://www.youtube.com/embed/${item.video}`}
+                          frameBorder="0"
+                          allowFullScreen
+                        />
                       </div>
-                    )
-                  }
-                </>
-              ))}
+                    )}
 
+                    {/* SIMILAIRES */}
+                    <h3>Jeux Similaires</h3>
 
-            </div>
+                    {gameData.map((i, idx) => (
+                      article.genre === i.genre && (
+                        <div key={idx}>
+                          <Link to={`/PC/${i.id}/${i.title}`}>
+                            <img src={i.imageUrl} alt={i.title} />
+                          </Link>
+
+                          <h4>
+                            <Link to={`/PC/${i.id}/${i.title}`}>
+                              {i.title}
+                            </Link>
+                            - {i.price}
+                          </h4>
+                        </div>
+                      )
+                    ))}
+
+                  </div>
+                </div>
+              )
+            ))}
 
           </div>
-        ) : (
-          // Code à exécuter lorsque item est null
-          <div>Chargement en cours...</div>
-        )}
+        </div>
       </div>
 
-      <div class="separator product-panel"></div>
-      <div class="separator product-panel"></div>
-
       <Footer />
-
-      {/* <h2>{article.title}</h2>
-
-     <img src={article.imageUrl} alt="" />
-     {article.news && article.news.map((item, id) => ( 
-      <>
- { item.news_id != news_id ? (
-  null ) :
-  (
-    <div className="div">
-    <h1>{item.title}</h1>
-    <img src={item.imageUrl} alt="" />
-    <p>{item.new}</p>
-    </div>
-  )
- }
-      </>
-     ))} */}
-
-
-
-
-
     </div>
   );
 }
